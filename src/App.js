08fc7bda22d06ce90eebf2df7448b2099f8f7e77
Avishya Thummalakuntla <avishya.t@gmail.com>
2023-02-05 17:33:54 -0500

    added Header.js for Task Tracker and imported in App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
+import Header from "./components/Header";
+
 function App() {
 
-  const name = 'Manoj';
-  const x = false;
-
-return (
+  return (
     <div className="container">
-      <h1>Hello from React</h1>
-      <h2>Hello {name}</h2>
-      <h2>Hello {1+1}</h2>
-      <h2>Hello {x ? 'Yes' : 'No'}</h2>
+      <Header />
     </div>
   );
 }
