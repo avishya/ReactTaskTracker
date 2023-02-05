3ac4e69082875b8817bd4d24c51e66af281c7bd8
Avishya Thummalakuntla <avishya.t@gmail.com>
2023-02-05 18:15:27 -0500

    adding style as const to h1

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types'
 const Header = ({ title }) => {
   return (
     <header>
-      <h1 style={{color:'red', backgroundColor:'green'}}>{title}</h1>
+      <h1 style={headingStyle}>{title}</h1>
     </header>
   )
 }
@@ -17,5 +17,9 @@ Header.propTypes = {
     title: PropTypes.string.isRequired,    
 }
 
+const headingStyle = {
+    color:'red', 
+    backgroundColor:'black'
+}
 
 export default Header
