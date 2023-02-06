8d8eb238428d6a79eb877cb909c60d09694f66fa
Avishya Thummalakuntla <avishya.t@gmail.com>
2023-02-05 20:04:42 -0500

    removed the harcoded h3 for eachTask in Tasks.js and moved that to Task.js and passed each task as prop

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,8 +1,10 @@
+import Task from "./Task";
+
 const Tasks = ({ tasks }) => {
   return (
     <>
-      {tasks.map((task) => (
-        <h3 key={task.id}>{task.text}</h3>
+      {tasks.map((eachTask) => (
+        <Task key={eachTask.id} task={eachTask} />
       ))}
     </>
   );
