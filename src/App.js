98c69e4c556668a750a2279efbf8593b08a2e8a1
Avishya Thummalakuntla <avishya.t@gmail.com>
2023-02-06 02:12:27 -0500

    useEffect() to load data by calling http://localhost:5000/tasks during page load

--- src/App.js
+++ src/App.js
@@ -2,11 +2,20 @@ import React from "react";
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
 import AddTask from "./components/AddTask";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
   const [showAddTask, setShowAddTask] = useState(false);
   const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    const fetchTasks = async () => {
+      const res = await fetch("http://localhost:5000/tasks");
+      const data = await res.json();
+      console.log(data);
+    };
+    fetchTasks();
+  }, []);
 
   //Save Task
   const saveTask = (task) => {
