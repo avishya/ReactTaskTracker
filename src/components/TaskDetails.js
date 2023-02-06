7729c8af8aff724a62c93696ad5f6b9ec83179c8
Avishya Thummalakuntla <avishya.t@gmail.com>
2023-02-06 04:10:09 -0500

    usage of Navigate during status=404

--- src/components/TaskDetails.js
+++ src/components/TaskDetails.js
@@ -19,11 +19,19 @@ const TaskDetails = () => {
       const res = await fetch(`http://localhost:5000/tasks/${params.id}`);
       const data = await res.json();
 
+      if (res.status === 404) {
+        setError("Task not found");
+      }
+
       setTask(data);
       setLoading(false);
     };
     fetchTask();
   });
+
+  if (error) {
+    return <Navigate to="/" />;
+  }
 
   return loading ? (
     <h3>Loading...</h3>
