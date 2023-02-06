9972fc62a748199be02f073702b6219643443185
Avishya Thummalakuntla <avishya.t@gmail.com>
2023-02-06 03:27:02 -0500

    useLocation to hide Add during About page

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,24 @@
 import PropTypes from "prop-types";
 import Button from "./Button";
+import { useLocation } from "react-router-dom";
 
 const Header = ({ title, onClickAddTask, showAdd }) => {
   const onClickAction = () => {
     console.log("Clicked111");
   };
 
+  const location = useLocation();
+
   return (
     <header className="header">
       <h1>{title}</h1>
-      <Button
-        color={showAdd ? "red" : "green"}
-        text={showAdd ? "Close" : "Add"}
-        onClickProp={onClickAddTask}
-      />
+      {location.pathname === "/" && (
+        <Button
+          color={showAdd ? "red" : "green"}
+          text={showAdd ? "Close" : "Add"}
+          onClickProp={onClickAddTask}
+        />
+      )}
     </header>
   );
 };
