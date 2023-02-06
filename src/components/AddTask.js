96fefe950eebef4b7d0a1999f7869a8733f4e541
Avishya Thummalakuntla <avishya.t@gmail.com>
2023-02-05 22:04:31 -0500

    Added AddTask.js with 3 fields in form & submit button without style/className and added this new file to App.js

---
+++ src/components/AddTask.js
@@ -0,0 +1,22 @@
+const AddTask = () => {
+  return (
+    <form>
+      <div>
+        <label>Task</label>
+        <input type="text" placeholder="Add Task" />
+      </div>
+      <div>
+        <label>Day & Time</label>
+        <input type="text" placeholder="Add Day & Time" />
+      </div>
+      <div>
+        <label>Set Reminder</label>
+        <input type="checkbox" />
+      </div>
+
+      <input type="submit" value="Save Task" />
+    </form>
+  );
+};
+
+export default AddTask;
